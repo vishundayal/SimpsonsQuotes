9923180f51af473374187c0c92995e4957b1b607
Vishun Dayal <vishundayal@Vishuns-MacBook-Pro.local>
2024-08-03 20:10:25 +0530

    First commit

---
+++ SimpsonsQuotes/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  SimpsonsQuotes
+//
+//  Created by Vishun Dayal on 03/08/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+    }
+
+}
+
